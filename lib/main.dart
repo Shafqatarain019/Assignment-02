5411b3f1d07965470782288440e1ee6812e00196
Shafqat Arain <engr.shafqatarain019@gmail.com>
2021-05-30 15:10:39 +0500

    Work Complete

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Login Page")),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                color: Colors.amber,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 200,
+                color: Colors.amber,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
